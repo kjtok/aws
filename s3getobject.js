938a7dcb0a65b68d05f7b2efb1849aa11065161b
Kenji Tokunaga <kjtokdg@gmail.com>
2018-02-17 09:10:29 +0900

    add s3getobject.js s3deleteobject.js

--- s3getobject.js
+++ s3getobject.js
@@ -4,18 +4,13 @@ AWS.config.region = 'ap-northeast-1';
 var bucketname = process.argv[2];
 var filename = process.argv[3];
 
-var fs = require('fs');
-var data = fs.createReadStream(filename);
-
 var params = {
     Bucket: bucketname,
     Key: filename,
-    Body: data
 };
 
 var s3 = new AWS.S3();
-s3.putObject(params, function(err, data) {
+s3.getObject(params, function(err, data) {
     if (err) console.log(err, err.stack);
     else console.log(data);
 });
-
